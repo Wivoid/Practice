6f16c6c10ca6bc8a375724a746a6b9e5129bc1b3
Wivoid <obama.pokaji.siski@gmail.com>
2025-12-03 00:45:56 +0200

    Time Manager - Layout Modification

--- Time_Manager.py
+++ Time_Manager.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QPushButton,
-                        QLabel, QTextEdit, QHBoxLayout, QVBoxLayout, QGridLayout)
+                        QLabel, QTextEdit, QHBoxLayout, QVBoxLayout)
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -14,12 +14,24 @@ class MainWindow(QMainWindow):
         self.central_widg = QWidget()
         self.setCentralWidget(self.central_widg)
 
-        self.grid = QGridLayout(self.central_widg)
-        self.grid.setColumnStretch(0,1)
-        self.grid.setColumnStretch(3,1)
+        self.hbox = QHBoxLayout()
+        self.vbox = QVBoxLayout()
+
+        self.vbox.addStretch(3)
+
+        self.vbox.addLayout(self.hbox)
+        self.central_widg.setLayout(self.vbox)
+
+        self.vbox.addStretch(1)
+
 
         self.hello_button = QPushButton("Hello", self)
         self.info_button = QPushButton("Info", self)
+
+        self.hbox.addWidget(self.info_button)
+        self.hbox.addWidget(self.hello_button)
+
+
 
         self.central_widg.setStyleSheet("""
             QPushButton {
@@ -28,8 +40,7 @@ class MainWindow(QMainWindow):
                                         
         """)
 
-        self.grid.addWidget(self.info_button, 1,1,1,1, Qt.AlignCenter)
-        self.grid.addWidget(self.hello_button, 1,2,1,1, Qt.AlignCenter)
+
 
 
 if __name__ == "__main__":
