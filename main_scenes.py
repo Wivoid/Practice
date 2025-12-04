acd7ebfdc086cf42b50b7c9d05ef1cd92224c0a6
Wivoid <obama.pokaji.siski@gmail.com>
2025-12-04 23:59:43 +0200

    Time Manager - Window Changes

--- main_scenes.py
+++ main_scenes.py
@@ -1,26 +1,17 @@
 import sys
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QPushButton,
-                        QLabel, QTextEdit, QHBoxLayout, QVBoxLayout, QStackedWidget)
-from main_scenes import Scene1
+from PyQt5.QtWidgets import (QWidget, QPushButton, QApplication, QStackedWidget,
+                        QLabel, QTextEdit, QHBoxLayout, QVBoxLayout, QMainWindow)
 
-class MainWindow(QMainWindow):
+class Scene1(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Time Manager")
         self.setGeometry(800,500,800,500)
         self.initUI()
-
-    
 
     def initUI(self):
         self.stack = QStackedWidget()
         self.setCentralWidget(self.stack)
-
-        self.main_scene = Scene1()
-
-        self.stack.addWidget(self.main_scene)
-        self.stack.setCurrentIndex(0)
 
         self.hbox = QHBoxLayout()
         self.vbox = QVBoxLayout()
@@ -49,12 +40,3 @@ class MainWindow(QMainWindow):
                 }
                                         
         """)
-
-
-
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec_()) 
