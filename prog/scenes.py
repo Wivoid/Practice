e82372e316525f95905d193edda4bdc0b8ac02bb
Wivoid <obama.pokaji.siski@gmail.com>
2025-12-08 00:33:22 +0200

    Time Manager - Focus Scene Boxes

--- prog/scenes.py
+++ prog/scenes.py
@@ -14,8 +14,10 @@ class Scene2(QWidget):
         self.recent_box = QWidget()
         self.statics_box = QWidget()
 
+
         self.middle_layout = QHBoxLayout()
 
+        # Time-Box
 
         self.time_text = QLabel("00:00", self)
         self.time_text.setAlignment(Qt.AlignCenter)
@@ -23,21 +25,29 @@ class Scene2(QWidget):
         self.time_layout = QVBoxLayout(self.focus_box)
         self.time_layout.addWidget(self.time_text)
         
+        
+        # Recent-Box
 
         self.recent_text = QLabel("Recent Activity", self)
         self.recent_text.setAlignment(Qt.AlignCenter)
 
         self.recent_layout = QVBoxLayout(self.recent_box)
-        self.recent_layout.addLayout(self.middle_layout)
         self.recent_layout.addWidget(self.recent_text)
 
+        # Statics-Box
+
         self.statics_text = QLabel("Statistics", self)
-        self.recent_text.setAlignment(Qt.AlignCenter)
+        self.statics_text.setAlignment(Qt.AlignCenter)
 
         self.statics_layout = QVBoxLayout(self.statics_box)
-        self.statics_layout.addLayout(self.middle_layout)
+        self.middle_layout.addLayout(self.statics_layout)
         self.statics_layout.addWidget(self.statics_text)
 
+
+        self.middle_layout.addWidget(self.statics_box)
+        self.middle_layout.addWidget(self.recent_box)
+
+        #Connections
 
         self.focus_box.setObjectName("TimerFrame")
         self.recent_box.setObjectName("RecentFrame")
@@ -45,10 +55,11 @@ class Scene2(QWidget):
 
 
         self.time_text.setStyleSheet("font: 30px;")
+        self.recent_text.setStyleSheet("font: 30px;")
+        self.statics_text.setStyleSheet("font: 30px;")
 
         self.vbox.insertWidget(0, self.focus_box)
-        self.vbox.insertWidget(1, self.recent_box)
-        self.vbox.insertWidget(2, self.statics_box)
+        self.vbox.insertLayout(1, self.middle_layout)
 
     def initUI(self):
 
@@ -61,7 +72,7 @@ class Scene2(QWidget):
         self.add_button = QPushButton("Add Subject", self)
         self.return_button = QPushButton("Return", self)
 
-        self.vbox.addStretch(1)
+        
 
         self.hbox.addWidget(self.return_button)
         self.hbox.addWidget(self.add_button)
