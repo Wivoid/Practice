268581ba2312fcaac50ba99b852ca53489f5f319
Wivoid <obama.pokaji.siski@gmail.com>
2025-12-08 15:37:36 +0200

    Time Manager - Statistics; Recent Boxes

--- prog/scenes.py
+++ prog/scenes.py
@@ -1,32 +1,40 @@
 import sys
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QWidget, QPushButton, QApplication,
-                             QLabel, QTextEdit, QHBoxLayout, QVBoxLayout)
+                             QLabel, QTextEdit, QHBoxLayout, QVBoxLayout, QSizePolicy)
 
 class Scene2(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
-        self.Time_Box()
-
-    def Time_Box(self):
-        self.focus_box = QWidget()
-        self.recent_box = QWidget()
-        self.statics_box = QWidget()
-
-
         self.middle_layout = QHBoxLayout()
 
-        # Time-Box
+        self.Time_Box()
+        self.Recent_Box()
+        self.Statics_Box()
+        
+        self.vbox.insertLayout(1, self.middle_layout)
+
+    def Time_Box(self):
+        self.time_box = QWidget()
+        
 
         self.time_text = QLabel("00:00", self)
         self.time_text.setAlignment(Qt.AlignCenter)
 
-        self.time_layout = QVBoxLayout(self.focus_box)
+        self.time_layout = QVBoxLayout(self.time_box)
         self.time_layout.addWidget(self.time_text)
-        
-        
-        # Recent-Box
+
+        self.vbox.insertWidget(0, self.time_box)
+
+
+        self.time_text.setStyleSheet("font: 30px;")
+
+        self.time_box.setObjectName("TimerFrame")
+        self.time_box.setFixedHeight(50)
+
+    def Recent_Box(self):
+        self.recent_box = QWidget()
 
         self.recent_text = QLabel("Recent Activity", self)
         self.recent_text.setAlignment(Qt.AlignCenter)
@@ -34,7 +42,14 @@ class Scene2(QWidget):
         self.recent_layout = QVBoxLayout(self.recent_box)
         self.recent_layout.addWidget(self.recent_text)
 
-        # Statics-Box
+
+        self.recent_text.setStyleSheet("font: 30px;")
+        self.recent_box.setObjectName("RecentFrame")
+
+        self.middle_layout.addWidget(self.recent_box)
+
+    def Statics_Box(self):
+        self.statics_box = QWidget()
 
         self.statics_text = QLabel("Statistics", self)
         self.statics_text.setAlignment(Qt.AlignCenter)
@@ -44,22 +59,11 @@ class Scene2(QWidget):
         self.statics_layout.addWidget(self.statics_text)
 
 
-        self.middle_layout.addWidget(self.statics_box)
-        self.middle_layout.addWidget(self.recent_box)
-
-        #Connections
-
-        self.focus_box.setObjectName("TimerFrame")
-        self.recent_box.setObjectName("RecentFrame")
+        self.statics_text.setStyleSheet("font: 30px;")
         self.statics_box.setObjectName("StaticsFrame")
 
+        self.middle_layout.addWidget(self.statics_box)
 
-        self.time_text.setStyleSheet("font: 30px;")
-        self.recent_text.setStyleSheet("font: 30px;")
-        self.statics_text.setStyleSheet("font: 30px;")
-
-        self.vbox.insertWidget(0, self.focus_box)
-        self.vbox.insertLayout(1, self.middle_layout)
 
     def initUI(self):
 
@@ -72,7 +76,6 @@ class Scene2(QWidget):
         self.add_button = QPushButton("Add Subject", self)
         self.return_button = QPushButton("Return", self)
 
-        
 
         self.hbox.addWidget(self.return_button)
         self.hbox.addWidget(self.add_button)
