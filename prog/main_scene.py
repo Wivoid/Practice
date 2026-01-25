842ca544d12f76b64efdff03b9dfbe811e35b361
Wivoid <obama.pokaji.siski@gmail.com>
2026-01-25 13:40:53 +0200

    style: Main Window detalization

--- prog/main_scene.py
+++ prog/main_scene.py
@@ -1,7 +1,7 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QWidget, QPushButton,QLabel,
                              QHBoxLayout, QVBoxLayout, QGraphicsDropShadowEffect,QApplication)
-from PyQt5.QtGui import QColor
+from PyQt5.QtGui import QColor, QBrush
 
 import sys
 
@@ -27,9 +27,15 @@ class Scene1(QWidget):
         self.info_button = QPushButton("Info", self)
 
         self.hbox.addWidget(self.info_button)
-        self.hbox.addWidget(self.start_button)
+        self.info_button.setMinimumSize(200,80)
+        self.info_button.setMaximumSize(800,100)
 
-        self.vbox.addWidget(self.hello_text)
+        self.hbox.addWidget(self.start_button)
+        self.start_button.setMinimumSize(200,80)
+        self.start_button.setMaximumSize(800,80)
+
+        self.vbox.addWidget(self.hello_text, alignment=Qt.AlignCenter)
+        self.hello_text.setFixedSize(310,100)
         self.hello_text.setAlignment(Qt.AlignCenter)
 
         self.vbox.addStretch(2)
@@ -38,18 +44,26 @@ class Scene1(QWidget):
         self.vbox.addStretch(1)
 
     def apply_shadow(self, widget):
+        
         shadow = QGraphicsDropShadowEffect()
-        shadow.setBlurRadius(50)
+        if widget == self.hello_text:
+            color = (QColor(0,0,100,50))
+            shadow.setBlurRadius(30)
+        else:
+            color = (QColor(0,0,100,110))
+            shadow.setBlurRadius(50)
         shadow.setXOffset(5)
         shadow.setYOffset(5)
 
-        color = (QColor(0,0,100,110))
         shadow.setColor(color)
         widget.setGraphicsEffect(shadow)
 
     def style(self):
+        
+
         self.apply_shadow(self.info_button)
         self.apply_shadow(self.start_button)
+        self.apply_shadow(self.hello_text)
 
         self.setStyleSheet("""
             QWidget{
@@ -59,18 +73,21 @@ class Scene1(QWidget):
             QPushButton {
                     margin-left: 30px;
                     margin-right: 30px;
-                    font-size: 40px;
-                    color: hsl(209, 20%, 28%);
+                    font-size: 50px;
+                    font-weight: bold;
+                    color: hsl(209, 56%, 70%);
                     background-color: hsl(209, 87%, 86%);
                     border: 4px outset hsl(209, 25%, 63%);
                     border-radius: 15px;
                         }
                            
             QPushButton:hover {
+                    color: hsl(209, 45%, 60%);
                     background-color: hsl(209, 70%, 83%);
                         }
                            
             QLabel {
+                    border-radius: 20px;
                     color: #56728c;
                     font-size: 50px;
                     }
